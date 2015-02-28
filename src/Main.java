63b4432ea3d45303f25e993ee17ec79dd62e6bd0
Nickos <Nickos@Rambou>
2015-02-28 05:46:57 +0200

    First working version

---
+++ src/Main.java
@@ -0,0 +1,178 @@
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Base64;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/*
+ * The MIT License
+ *
+ * Copyright 2015 Nikolaos Bousios (Rambou).
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+/**
+ *
+ * @author Nikolaos Bousios (Rambou)
+ */
+public class Main {
+
+    public static void main(String[] Args) {
+
+        //Checking if at least 1 argument has been given
+        if (Args.length < 1) {
+            System.exit(0);
+        }
+        //Checking if the argument is a valid path of Directory
+        if (!(new File(Args[0])).isDirectory()) {
+            System.exit(-1);
+        }
+
+        //Create the mbox file to store mails from txt
+        File mboxfile = null;
+
+        try {
+            //Checking if user has given the mbox file name
+            if (Args.length > 1) {
+                mboxfile = new File(System.getProperty("user.dir") + "\\" + Args[1]);
+            } else {
+                mboxfile = new File(System.getProperty("user.dir") + "\\All_mails.mbox");
+            }
+
+            //If file exist then gets deleted
+            if (mboxfile.exists()) {
+                mboxfile.delete();
+            }
+            //File is created
+            mboxfile.createNewFile();
+        } catch (IOException ex) {
+            System.out.println(ex);
+            System.exit(-1);
+        }
+
+        //Getting all .txt files
+        ArrayList<File> Files = listFilesAllFiles(Args[0], new ArrayList<>());
+
+        for (File f : Files) {
+
+            String _from = null,
+                    From = "From MAILER-DAEMON",
+                    Date = null,
+                    to = null,
+                    Subject = null,
+                    Body = null,
+                    ContentType = "Content-Type: text/plain; charset=utf-8",
+                    ContentTransferEncoding = "Content-Transfer-Encoding: base64";
+
+            //Read the txt file first and contruct the message into mbox format
+            try (BufferedReader br = new BufferedReader(new InputStreamReader(
+                    new FileInputStream(f), "UTF8"))) {
+                StringBuilder sb = new StringBuilder();
+                String line = br.readLine();
+
+                while (line != null) {
+                    if (line.contains("Date:")) {
+                        Date = line.replace("Date:", "").trim();
+                    } else if (line.contains("From:")) {
+                        _from = line;
+                    } else if (line.contains("Subject:")) {
+                        Subject = line;
+                    } else if (line.contains("To:")) {
+                        to = line;
+                    } else {
+                        sb.append(line);
+                        sb.append(System.lineSeparator());
+                    }
+                    line = br.readLine();
+                }
+                //Convert body in base64 using UTF-8 charset
+                Body = Base64.getEncoder().encodeToString(sb.toString().getBytes(StandardCharsets.UTF_8));
+
+            } catch (Exception ex) {
+                Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
+            //Now write the content at the end of the mbox file
+            try {
+
+                OutputStreamWriter writer = new OutputStreamWriter(
+                        new FileOutputStream(mboxfile, true));
+                BufferedWriter fbw = new BufferedWriter(writer);
+                fbw.write(From + " " + Date);
+                fbw.newLine();
+                fbw.write(_from);
+                fbw.newLine();
+                fbw.write(to);
+                fbw.newLine();
+                fbw.write(Subject);
+                fbw.newLine();
+                fbw.write(ContentType);
+                fbw.newLine();
+                fbw.write(ContentTransferEncoding);
+                fbw.newLine();
+                fbw.newLine();
+                fbw.write(Body);
+                fbw.newLine();
+                fbw.newLine();
+                fbw.close();
+            } catch (Exception e) {
+                System.out.println("Error: " + e.getMessage());
+            }
+        }
+
+    }
+
+    //A recursive function to get all files from the given directory and its subdirectories
+    public static ArrayList<File> listFilesAllFiles(String directoryName, ArrayList<File> Files) {
+
+        //Getting all files of the Directory
+        File Directory = new File(directoryName);
+        File[] listOfFiles = Directory.listFiles();
+
+        for (File file : listOfFiles) {
+            if (file.isFile()) {
+                if (isFileTxt(file)) {
+                    Files.add(new File(file.getAbsolutePath()));
+                }
+            } else if (file.isDirectory()) {
+                listFilesAllFiles(file.getAbsolutePath(), Files);
+            }
+        }
+
+        return Files;
+    }
+
+    private static boolean isFileTxt(File file) {
+        String name = file.getName();
+        int lastIndexOf = name.lastIndexOf(".");
+        if (lastIndexOf == -1) {
+            return false; // empty extension
+        }
+        return name.substring(lastIndexOf).equals(".txt");
+    }
+}
